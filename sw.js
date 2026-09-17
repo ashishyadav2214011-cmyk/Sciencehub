81a371217233d40ba3eb02270ce0d1890f37d4c4
Ashish <ashishyadav2214011@gmail.com>
2026-09-17 21:10:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="sciencehub-v69-homehero-icon";
+const CACHE="sciencehub-v70-functional-final";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./sciencehub-icon.png","./sciencehub-icon-192.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
